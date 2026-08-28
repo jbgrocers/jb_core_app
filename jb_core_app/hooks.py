1acf53fb2016900232311ba0da338d9a6f4e7a71
sachiingoel <sachingoel24@gmail.com>
2026-08-28 17:08:58 +0530

    Add initial app metadata to hooks.py

---
+++ jb_core_app/hooks.py
@@ -0,0 +1,8 @@
+from . import __version__ as app_version
+
+app_name = "jb_core_app"
+app_title = "JB Core App"
+app_publisher = "JB Grocers LLP"
+app_description = "JB Core App - Phase R shared server and client script estate for jh"
+app_email = "sachingoel24@gmail.com"
+app_license = "MIT"
